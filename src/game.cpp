8b3cc6db3b65d38abe4ec154dfdb458c6e2a5407
Francisco <franjonas98@hotmail.com>
2025-10-30 16:22:58 -0300

    Update main loop

--- src/game.cpp
+++ src/game.cpp
@@ -2,8 +2,43 @@
 
 namespace flappy
 {
+	void Init();
+	void Input();
+	void Update();
+	void Draw();
+
 	void RunGame()
 	{
+		Init();
 
+		while (!WindowShouldClose())
+		{
+			Input();
+			Update();
+			Draw();
+		}
+
+		CloseWindow();
 	}
+
+	void Init()
+	{
+		const int screenWidth = 1024;
+		const int screenHeight = 768;
+
+		InitWindow(screenWidth, screenHeight, "Aracnoids");
+	}
+
+	void Input()
+	{
+	}
+
+	void Update()
+	{
+	}
+
+	void Draw()
+	{
+	}
+
 }
