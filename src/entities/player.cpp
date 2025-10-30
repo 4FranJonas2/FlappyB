f50d0ae8201423d6224d84eb2f6fc89a45331eae
Francisco <franjonas98@hotmail.com>
2025-10-30 19:05:06 -0300

    Add player basic structure

--- src/entities/player.cpp
+++ src/entities/player.cpp
@@ -3,4 +3,62 @@
 namespace flappy
 {
 
+	void InitPlayer(Player& player)
+	{
+		player.playerFigure.x = 300.0f;
+		player.playerFigure.y = 500.0f;
+		player.playerFigure.width = 30.0f;
+		player.playerFigure.height = 40.0f;
+
+		player.playerHitbox.pos.x = 300.0f;
+		player.playerHitbox.pos.y = 500.0f;
+		player.playerHitbox.rad = 10.0f;
+
+		player.lives = 1;
+
+		player.moveUp = false;
+		player.moveDown = false;
+	}
+	void InputPlayer(Player& player)
+	{
+		if (IsKeyDown(KEY_W) && !player.moveDown)
+		{
+			player.moveUp = true;
+		}
+		if (IsKeyDown(KEY_S) && !player.moveUp)
+		{
+			player.moveDown = true;
+		}
+		if (!IsKeyDown(KEY_W))
+		{
+			player.moveUp = false;
+		}
+		if (!IsKeyDown(KEY_S))
+		{
+			player.moveDown = false;
+		}
+	}
+	void UpdatePlayer(Player& player)
+	{
+		if (player.moveUp)
+		{
+			player.playerFigure.x--;
+			player.playerHitbox.pos.x = player.playerFigure.x;
+		}
+		if (player.moveDown)
+		{
+			player.playerFigure.x++;
+			player.playerHitbox.pos.x = player.playerFigure.x;
+		}
+	}
+	void DrawPlayer(Player& player)
+	{
+		DrawRectangle(static_cast <int>(player.playerFigure.x),
+			static_cast <int>(player.playerFigure.y),
+			static_cast <int>(player.playerFigure.width),
+			static_cast <int>(player.playerFigure.height),RED);
+		DrawCircleLines(static_cast <int>(player.playerHitbox.pos.x),
+			static_cast <int>(player.playerHitbox.pos.y),
+			static_cast <int>(player.playerHitbox.rad), RED);
+	}
 }
--- src/entities/player.h
+++ src/entities/player.h
@@ -1,6 +1,22 @@
 #pragma once
 
+#include "../utilities.h"
+
 namespace flappy
 {
+	struct Player
+	{
+		Rectangle playerFigure;
+		Circle playerHitbox;
+		int lives;
+
+		bool moveUp;
+		bool moveDown;
+	};
+
+	void InitPlayer(Player& player);
+	void InputPlayer(Player& player);
+	void UpdatePlayer(Player& player);
+	void DrawPlayer(Player& player);
 
 }
